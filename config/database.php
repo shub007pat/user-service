4343d2d9c5efdeeec8e1ee2b670df47994842831
Shubham Patil <patil.shub555@gmail.com>
2023-11-14 12:18:04 +0530

    Initial commit

---
+++ config/database.php
@@ -0,0 +1,79 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'user_db';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo 'Connection Error: ' . $e->getMessage();
+        }
+        return $this->conn;
+    }
+
+    public function addUser($user) {
+        $query = 'INSERT INTO users (username, password, email, address) VALUES (:username, :password, :email, :address)';
+        $stmt = $this->getConnection()->prepare($query);
+    
+        // Bind parameters
+        $stmt->bindParam(':username', $user['username']);
+        $stmt->bindParam(':password', $user['password']);
+        $stmt->bindParam(':email', $user['email']);
+        $stmt->bindParam(':address', $user['address']);
+    
+        // Execute the query
+        if ($stmt->execute()) {
+            return $this->conn->lastInsertId();
+        } else {
+            return false;
+        }
+    }
+    
+    public function loginUser($credentials) {
+        $query = 'SELECT id, username, email, address FROM users WHERE username = :username AND password = :password';
+        $stmt = $this->getConnection()->prepare($query);
+    
+        // Bind parameters
+        $stmt->bindParam(':username', $credentials['username']);
+        $stmt->bindParam(':password', $credentials['password']);
+    
+        // Execute the query
+        $stmt->execute();
+    
+        // Fetch the user
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+        if ($user) {
+            return $user;
+        } else {
+            return false;
+        }
+    }
+
+    public function getUserDetails($id) {
+        $query = 'SELECT id, username, email, address FROM users WHERE id = :id';
+        $stmt = $this->getConnection()->prepare($query);
+    
+        // Bind parameters
+        $stmt->bindParam(':id', $id);
+    
+        // Execute the query
+        $stmt->execute();
+    
+        // Fetch the user
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+        if ($user) {
+            return $user;
+        } else {
+            return false;
+        }
+    }
+    
+}
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+// Include database configuration
+require 'config/database.php';
+
+// Get the HTTP method and path
+$method = $_SERVER['REQUEST_METHOD'];
+
+$request_uri = $_SERVER['REQUEST_URI'];
+
+// Extract the path from REQUEST_URI
+$path = parse_url($request_uri, PHP_URL_PATH);
+
+// Remove any base path from the path
+$base_path = '/user-service'; // Update this to your actual base path
+$path = str_replace($base_path, '', $path);
+
+// Parse the path to determine the request
+$request = explode('/', trim($path, '/'));
+
+
+// Connect to the database
+$db = new Database();
+
+if ($method === 'POST' && $request[1] === 'users') {
+    // Add a new user
+    $input = json_decode(file_get_contents('php://input'), true);
+    $user_id = $db->addUser($input);
+    echo json_encode(array('user_id' => $user_id));
+} elseif ($method === 'POST' && $request[1] === 'login') {
+    // User login
+    $input = json_decode(file_get_contents('php://input'), true);
+    $user = $db->loginUser($input);
+    if ($user) {
+        echo json_encode(array('message' => 'Login successful'));
+    } else {
+        echo json_encode(array('message' => 'Login failed'));
+    }
+} elseif ($method === 'GET' && $request[1] === 'users') {
+    // To get the user details, pass the id as a query parameter
+    $id = isset($_GET['id']) ? $_GET['id'] : null;
+    
+    if ($id) {
+        $user = $db->getUserDetails($id);
+        echo json_encode($user);
+    } else {
+        // Return an error or handle the case where id is missing
+        echo json_encode(array('error' => 'User ID is missing'));
+    }
+}
